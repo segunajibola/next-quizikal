baffe8e1290a5ffe0cb7c3bd8a200f24e5e7d9c0
Segun Ajibola <ajbl.segun@gmail.com>
2024-05-19 23:23:58 +0100

    addTanstack

--- components/Collaborators.jsx
+++ components/Collaborators.jsx
@@ -5,7 +5,7 @@ const Collaborators = ({ collaborator: { avatar_url, html_url, login, role_name 
     <div className="flex flex-col gap-1 justify-center items-center">
       <img
         src={avatar_url}
-        className="w-20 h-20 rounded-[50%]".
+        className="w-20 h-20 rounded-[50%]"
         alt={`${login}'s avatar`}
       />
       <a href={html_url} className="underline">
